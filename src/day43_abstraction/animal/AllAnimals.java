526015963ce38244997c66c16128e46f45f35876
otabek20041017 <otabek.yusupov20041017@gmail.com>
2022-09-13 01:20:28 +0500

    First_Java_Project created in 6 June 2022! Pushed in 13.09.2022!

---
+++ src/day43_abstraction/animal/AllAnimals.java
@@ -0,0 +1,25 @@
+package day43_abstraction.animal;
+
+public class AllAnimals {
+
+}
+
+class Cat extends Animal implements Playable, Petable{
+
+}
+
+class Dog extends Animal implements Friendly, Swimable {
+
+}
+class Tiger extends Animal implements Swimable {
+
+}
+class Shark extends Animal implements Swimable {
+
+}
+class Bird extends Animal implements Flyable{
+
+}
+class Duck extends Animal implements Flyable, Swimable {
+
+}
---
+++ src/day43_abstraction/animal/Friendly.java
@@ -0,0 +1,5 @@
+package day43_abstraction.animal;
+
+public interface Friendly extends Playable, Petable{
+    
+}
